6a95091142cde02bfd554e43c9b4bd4f2d3f562a
yaojun <940334249@qq.com>
2023-04-02 20:16:53 +0800

    bugfix: 简单代码一堆bug

--- student_system/student_expense_record.c
+++ student_system/student_expense_record.c
@@ -19,51 +19,100 @@ Student T[M];
 int total_num = 0;
 static int n = 0;
 void menu() {
-    printf("\t\t|----------------学生消费系统-------------- |\n");
-    printf("\t\t|\t1.录入学生信息                              |\n");
-    printf("\t\t|\t2.输出学生信息                              |\n");
-    printf("\t\t|\t3.查询学生信息                              |\n");
-    printf("\t\t|\t4.删除学生信息                              |\n");
-    printf("\t\t|\t5.修改学生信息                              |\n");
-    printf("\t\t|\t6.排序学生信息                              |\n");
-    printf("\t\t|\t7.统计学生信息                              |\n");
-    printf("\t\t|\t8.保存学生信息                              |\n");
-    printf("\t\t|\t9.读取学生信息                              |\n");
-    printf("\t\t|\t0.退出                                     |\n");
-    printf("\t\t|--------------------------------------------|\n\n");
-    printf("\t\t\t请选择(0-9):");
+    printf("==================学生消费系统==============\n");
+    printf("1.录入学生信息                              \n");
+    printf("2.输出学生信息                              \n");
+    printf("3.查询学生信息                              \n");
+    printf("4.删除学生信息                              \n");
+    printf("5.修改学生信息                              \n");
+    printf("6.排序学生信息                              \n");
+    printf("7.统计学生信息                              \n");
+    printf("8.保存学生信息                              \n");
+    printf("9.读取学生信息                              \n");
+    printf("0.退出                                     \n");
+    printf("==========================================\n");
+    printf("请选择(0-9):");
 }
 void add(){
-    puts("开始录入信息");
-    Student t;
-    puts("请输入姓名:");
-    scanf("%s", t.name);
-    puts("请输入学号:");
-    scanf("%d", &t.id);
-    puts("请输入消费金额:");
-    scanf("%lf", &t.amount);
-    puts("请输入消费用途:");
-    scanf("%s", t.kind);
-    puts("请输入消费日期:");
-    scanf("%s", t.date);
+    while(1){
+        puts("开始录入信息");
+        Student t;
+        puts("请输入姓名:");
+        scanf("%s", t.name);
+        puts("请输入学号:");
+        scanf("%d", &t.id);
+        puts("请输入消费金额:");
+        scanf("%lf", &t.amount);
+        puts("请输入消费用途:");
+        scanf("%s", t.kind);
+        puts("请输入消费日期:");
+        scanf("%s", t.date);
 
-    // 判重
-    for(int i = 0; i < total_num; i++) {
-        if(T[i].id == t.id){
-            puts("添加失败，已存在相同学号。");
-            return;
+        // 判重
+        for(int i = 0; i < total_num; i++) {
+            if(T[i].id == t.id){
+                puts("添加失败，已存在相同学号。");
+                return;
+            }
         }
+        T[total_num++] = t;
+        puts("新增成功");
+        puts("********");
+        puts("1. 继续添加");
+        puts("2. 退出");
+        puts("********");
+        int x;
+        scanf("%d", &x);
+        if(x == 2) break;
     }
-    T[total_num++] = t;
-    puts("新增成功");
 }
 void output() {
+    puts("姓名\t学号\t消费金额\t消费用途\t消费日期");
     for(int i = 0; i < total_num; i++) {
-        printf("姓名 = %s，学号 = %d，消费金额 = %lf, 消费用途 = %s，消费日期 = %s\n",
+        printf("%s\t%d\t%lf\t%s\t%s\n",
                T[i].name, T[i].id, T[i].amount, T[i].kind, T[i].date);
     }
 }
 
+void part_modify(){
+    puts("*******************");
+    puts("1. 修改姓名");
+    puts("2. 修改消费金额");
+    puts("3. 修改消费用途");
+    puts("4. 修改消费消费日期");
+    puts("*******************");
+    int x;
+    scanf("%d", &x);
+    puts("请输入学号:");
+    int id;
+    scanf("%d", &id);
+    int state = -1;
+    for(int i = 0; i < total_num; i++) {
+        if(T[i].id == id){
+            state = i;
+            break;
+        }
+    }
+    if(state == -1){
+        puts("学号不存在，修改失败！");
+    }
+    if(x == 1){
+        puts("请输入修改后的姓名:");
+        scanf("%s", T[state].name);
+        puts("修改成功!");
+    }else if(x == 2){
+        puts("请输入修改后的消费金额:");
+        scanf("%lf", &T[state].amount);
+        puts("修改成功!");
+    }else if(x == 3){
+        puts("请输入修改后的消费用途:");
+        scanf("%s", T[state].kind);
+    }else if(x == 4){
+        puts("请输入修改后的消费日期:");
+        scanf("%s", T[state].date);
+        puts("修改成功!");
+    }
+}
 void modify(){
     puts("*******************");
     puts("1. 全部修改");
@@ -71,7 +120,9 @@ void modify(){
     puts("*******************");
     int t;
     scanf("%d", &t);
-    if(t == 1 || t == 2) {
+    if(t == 2){
+        part_modify();
+    }else if(t == 1) {
         puts("请输入要修改的学号:");
         Student t;
         scanf("%d", &t.id);
@@ -85,6 +136,8 @@ void modify(){
                 scanf("%s", T[i].kind);
                 puts("请输入修改后的消费日期:");
                 scanf("%s", T[i].date);
+                puts("修改成功!");
+                return;
             }
         }
         puts("修改失败，学号不存在！");
@@ -166,13 +219,15 @@ void stat(){
     puts("*******************");
     int x;
     scanf("%d", &x);
-    int k = 0;
+
     if(x == 1) {
+        int k = 0;
         for(int i = 0; i < total_num; i++) {
             int j;
             for(j = 0; j < k; j++) {
                 if(strcmp(T[i].kind, kinds[j].name) == 0){
                     kinds[j].cnt++;
+                    break;
                 }
             }
             if(j == k){
@@ -186,17 +241,19 @@ void stat(){
             printf("消费用途 = %s，同类人数 = %d\n", kinds[i].name, kinds[i].cnt);
         }
     }else if(x == 2){
+        int k = 0;
         for(int i = 0; i < total_num; i++) {
             int j;
             for(j = 0; j < k; j++) {
                 if(strcmp(T[i].date, kinds[j].name) == 0){
                     kinds[j].cnt++;
+                    break;
                 }
             }
             if(j == k){
-                k++;
                 strcpy(kinds[k].name, T[i].date);
                 kinds[k].cnt = 1;
+                k++;
             }
         }
         // 输出
@@ -215,7 +272,7 @@ void sort() {
     scanf("%d", &x);
     if(x == 1) {
         for(int i = 0; i < total_num; i++) {
-            for(int j = 0; j + 1 < n; j++) {
+            for(int j = 0; j + 1 < total_num; j++) {
                 if(strcmp(T[j].name, T[j+1].name) > 0) {
                     Student t = T[j];
                     T[j] = T[j+1];
@@ -223,9 +280,10 @@ void sort() {
                 }
             }
         }
+        output();
     }else if(x == 2){
         for(int i = 0; i < total_num; i++) {
-            for(int j = 0; j + 1< n; j++) {
+            for(int j = 0; j + 1< total_num; j++) {
                 if(T[j].amount > T[j+1].amount) {
                     Student t = T[j];
                     T[j] = T[j+1];
@@ -233,6 +291,7 @@ void sort() {
                 }
             }
         }
+        output();
     }
 }
 void save(){
